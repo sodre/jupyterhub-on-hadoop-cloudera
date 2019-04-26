f5a6d0d3f7ac0aaf995a6032d70ddc2bb00197eb
Patrick Sodre <psodre@gmail.com>
2019-04-26 04:43:21 +0000

    Initial configuration

---
+++ README.md
@@ -0,0 +1,11 @@
+# jupyterhub-parcel
+
+```bash
+# Generate the jupyterhub environment
+conda env create .
+
+# Generate the parcel-meta file
+conda-env-to-parcel
+
+# Use conda-pack to generate the parcel tar.gz
+```
---
+++ meta/jupyterhub_env.sh
@@ -0,0 +1,16 @@
+# We can not use conda-unpack at this point because the process sourcing 
+# this file does not have write access to the parcel directory.
+if $PARCELS_ROOT != "/opt/cloudera/parcels"; then
+  cat <<EOF
+ERROR: Unsuported PARCELS_ROOT directory.
+  The JupyterHub parcel can only be installed in Cloudera's default 
+  parcel location (/opt/cloudera/parcels), but your parcels 
+  location is $PARCELS_ROOT. 
+
+  Please go to github.com/sodre/jupyterhub-parcel for instructions 
+  on how to regenerate this parcel to match your cluster configuration.
+EOF
+  exit 2
+fi
+
+source $PARCELS_ROOT/$PARCEL_DIRNAME/bin/activate
