93f606bea4838de3bdef9d98e5bb89d3d9739021
Patrick Sodré <psodre@gmail.com>
2019-04-28 16:43:07 -0400

    Stop and restart the service within build.sh   - build.sh is actually more like a "deploy.sh" script.

--- csd/build.sh
+++ csd/build.sh
@@ -15,6 +15,11 @@ curl -s http://localhost:7180/cmf/login/j_spring_security_check \
      -c .cookie -d "j_username=${CM_USR}&j_password=${CM_PSW}"
 
 # Reinstall the parcel
-curl -b .cookie -s http://localhost:7180/cmf/csd/refresh | jq .
+export CDH_SERVICE=jupyterhub
+curl -b .cookie -s -X POST http://localhost:7180/api/v17/clusters/cluster/services/${CDH_SERVICE}/commands/stop | jq || true
+
+curl -b .cookie -s http://localhost:7180/cmf/csd/refresh | jq .data.invalidCsds
 curl -b .cookie -s http://localhost:7180/cmf/csd/uninstall?csdName=$(basename $(pwd))\&force=true | jq .
 curl -b .cookie -s http://localhost:7180/cmf/csd/install?csdName=$(basename $(pwd)) | jq .
+
+curl -b .cookie -s -X POST http://localhost:7180/api/v17/clusters/cluster/services/${CDH_SERVICE}/commands/start | jq || true
