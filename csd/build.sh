1fe3da56b2d33f55baa45f8c9ec7c29bde2d6ed5
Patrick Sodré <psodre@gmail.com>
2019-04-28 15:39:35 -0400

    A runnable ConfigurableHTTPProxy in ClouderaManager

--- csd/build.sh
+++ csd/build.sh
@@ -8,14 +8,13 @@ convert -resize 16x16 images/logo.png images/icon.png
 yq . descriptor/service.sdl.yaml > descriptor/service.sdl
 validator.sh -s descriptor/service.sdl
 
-# Restart/Reload the CSD
-if [[ ! -e .cookie ]]; then
-  CM_USR=admin
-  CM_PSW=$(sudo mdata-get admin_pw)
-  curl -s http://localhost:7180/cmf/login/j_spring_security_check \
-       -c .cookie -d "j_username=${CM_USR}&j_password=${CM_PSW}"
-fi
+# Login to Cloudera Manager
+CM_USR=admin
+CM_PSW=$(sudo mdata-get admin_pw)
+curl -s http://localhost:7180/cmf/login/j_spring_security_check \
+     -c .cookie -d "j_username=${CM_USR}&j_password=${CM_PSW}"
 
 # Reinstall the parcel
-curl -b .cookie -s http://localhost:7180/cmf/csd/uninstall?csdName=JUPYTERHUB-0.0.1 | jq .
-curl -b .cookie -s http://localhost:7180/cmf/csd/install?csdName=JUPYTERHUB-0.0.1 | jq .
+curl -b .cookie -s http://localhost:7180/cmf/csd/refresh | jq .
+curl -b .cookie -s http://localhost:7180/cmf/csd/uninstall?csdName=$(basename $(pwd))\&force=true | jq .
+curl -b .cookie -s http://localhost:7180/cmf/csd/install?csdName=$(basename $(pwd)) | jq .
--- csd/scripts/start-proxy.sh
+++ csd/scripts/start-proxy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euf
 
-exec configurable-http-proxy
+exec configurable-http-proxy $(cat node-http-proxy.gflags)
