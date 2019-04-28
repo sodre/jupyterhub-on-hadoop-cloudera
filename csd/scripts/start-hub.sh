2e77f5b52b3101df18463c5c86fe18bfb6e2de55
Patrick Sodré <psodre@gmail.com>
2019-04-28 18:04:11 -0400

    Add parameter for Hub cookie secret

--- csd/scripts/start-hub.sh
+++ csd/scripts/start-hub.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -euf
 
+# Convert JPY_COOKIE_SECRET to Hex and add 3 characters
+export JPY_COOKIE_SECRET=$(hexdump -ve '1/1 "%.2x"' <<< "$JPY_COOKIE_SECRET")
+
 exec jupyterhub $(cat hub.gflags)
